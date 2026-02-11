c1af3b6ddeceb46b183563c63f91a374f772565a
Sarjak Bhandari <bhandarisarjak@gmail.com>
2026-02-11 13:15:25 +0545

    updated dtos and some error fixing related to reviews

---
+++ src/dtos/review.dtos.ts
@@ -0,0 +1,23 @@
+// src/dtos/review.dto.ts
+export type CreateReviewDto = {
+  title: string;
+  description?: string;
+  sentiments?: string[] | string;
+  productName?: string;
+  productImage?: string;
+  authorId: string;
+  authorLocation?: string;
+};
+
+export type UpdateReviewDto = {
+  title?: string;
+  description?: string;
+  sentiments?: string[] | string;
+  productName?: string;
+  productImage?: string;
+  authorLocation?: string;
+};
+
+export type LikeDto = {
+  userId: string;
+};
